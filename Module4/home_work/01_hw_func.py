38d1420ddab94e706f585cbf86e91950fc0622c5
Qvshinov <102471390+Qvshinov@users.noreply.github.com>
2022-04-10 20:01:22 +0300

    Update 01_hw_func.py

--- Module4/home_work/01_hw_func.py
+++ Module4/home_work/01_hw_func.py
@@ -3,8 +3,11 @@
 # Билет считается счастливым, если сумма его первых и последних цифр равны.
 
 def lucky_ticket(ticket_number):
-    # TODO: your code here
-    pass
+    lst = list(str(ticket_number))
+    if len(lst) == 6:
+        summa1 = int(lst[0]) + int(lst[1]) + int(lst[2])
+        summa2 = int(lst[3]) + int(lst[4]) + int(lst[5])
+        return summa1 == summa2
 
 
 # Тестируем функцию
